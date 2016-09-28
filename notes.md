4983d60238299b9eb81626488684fbe75c26847d
Jay Harris <jay.harris@namely.com>
2016-09-28 15:11:24 -0400

    it should run 5 alphas for every 1 bravo

--- notes.md
+++ notes.md
@@ -11,3 +11,7 @@ end
 ~~~
 
 Sidekiq starts the Alpha workers first, and it doesn't start the Bravo workers until all of the Alpha workers have started. However, it doesn't run the Alpha workers in the order that they're created, and it doesn't run the Bravo workers in the order that they're created.
+
+- - -
+
+Now we have two queues, the default one with a priority of 5 and a "slow" queue with a priority of 1. Both queues have at least 25 jobs on them, so either one would consume all of Sidekiq's threads. It looks like sidekiq pulls jobs off the default queue at slightly more than 5x the rate that pulls jobs off the slow queue. _Notably, the slow queue is not completely impeded by the overwhelmed fast queue._
--- worker.rb
+++ worker.rb
@@ -28,33 +28,9 @@ module MyWorkers
     include Sidekiq::Worker
     sidekiq_options queue: 'slow'
 
-    VICTORY = %{
-  _______AAAA_______________AAAA________
-         VVVV               VVVV
-         (__)               (__)
-          \ \               / /
-           \ \   \\|||//   / /
-            > \   _   _   / <
-   hang      > \ / \ / \ / <
-    in        > \\_o_o_// <
-    there...   > ( (_) ) <
-                >|     |<
-               / |\___/| \
-               / (_____) \
-               /         \
-                /   o   \
-                 ) ___ (
-                / /   \ \
-               ( /     \ )
-               ><       ><
-              ///\     /\\\
-              '''       '''
-    }
-
     def perform(id)
       puts "Starting thingy #{self.class.to_s}[#{id}]/job[#{self.jid}]"
-      puts VICTORY
-      sleep 5
+      sleep 6
       puts "Finished thingy #{self.class.to_s}[#{id}]/job[#{self.jid}]"
     end
   end
@@ -75,20 +51,23 @@ class DoIt
       MyWorkers::WorkerAlpha.perform_async(alphas)
     end
 
-    puts "creating 5 bravos on the slow queue"
-    5.times do
+    puts "creating 25 bravos on the slow queue"
+    25.times do
       bravos += 1
       MyWorkers::WorkerBravo.perform_async(bravos)
     end
 
     puts "run for three minutes."
-    puts "add an alpha to the default queue every second so the default queue always consumes all 25 threads"
+    puts "add an alpha and bravo to their respective queues every second to keep them full"
     start = Time.now.to_i
     loop do
       break if Time.now.to_i - start > 180
 
       alphas += 1
       MyWorkers::WorkerAlpha.perform_async(alphas)
+
+      bravos += 1
+      MyWorkers::WorkerBravo.perform_async(bravos)
     end
 
     puts "client done!"
