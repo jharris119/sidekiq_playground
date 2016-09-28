b3cb6167d6113ae95963c24bfd036d54f910180c
Jay Harris <jay.harris@namely.com>
2016-09-28 14:54:39 -0400

    attempting to overwhelm the high-priority queue so the jobs on the low-priority queue never execute

--- worker.rb
+++ worker.rb
@@ -19,7 +19,7 @@ module MyWorkers
 
     def perform(id)
       puts "Starting thingy #{self.class.to_s}[#{id}]/job[#{self.jid}]"
-      sleep 5
+      sleep 6
       puts "Finished thingy #{self.class.to_s}[#{id}]/job[#{self.jid}]"
     end
   end
@@ -28,8 +28,32 @@ module MyWorkers
     include Sidekiq::Worker
     sidekiq_options queue: 'slow'
 
+    VICTORY = %{
+  _______AAAA_______________AAAA________
+         VVVV               VVVV
+         (__)               (__)
+          \ \               / /
+           \ \   \\|||//   / /
+            > \   _   _   / <
+   hang      > \ / \ / \ / <
+    in        > \\_o_o_// <
+    there...   > ( (_) ) <
+                >|     |<
+               / |\___/| \
+               / (_____) \
+               /         \
+                /   o   \
+                 ) ___ (
+                / /   \ \
+               ( /     \ )
+               ><       ><
+              ///\     /\\\
+              '''       '''
+    }
+
     def perform(id)
       puts "Starting thingy #{self.class.to_s}[#{id}]/job[#{self.jid}]"
+      puts VICTORY
       sleep 5
       puts "Finished thingy #{self.class.to_s}[#{id}]/job[#{self.jid}]"
     end
@@ -41,13 +65,30 @@ class DoIt
     puts "Queues: #{Sidekiq::Queue.all}"
 
     Redis.new.flushdb
-    puts "Creating 40 alphas and 5 bravos"
 
-    40.times do |t|
-      MyWorkers::WorkerAlpha.perform_async(t)
+    alphas = 0
+    bravos = 0
+
+    puts "creating 25 alphas to consume all threads on the default queue"
+    25.times do |t|
+      alphas += 1
+      MyWorkers::WorkerAlpha.perform_async(alphas)
     end
-    5.times do |t|
-      MyWorkers::WorkerBravo.perform_async(t)
+
+    puts "creating 5 bravos on the slow queue"
+    5.times do
+      bravos += 1
+      MyWorkers::WorkerBravo.perform_async(bravos)
+    end
+
+    puts "run for three minutes."
+    puts "add an alpha to the default queue every second so the default queue always consumes all 25 threads"
+    start = Time.now.to_i
+    loop do
+      break if Time.now.to_i - start > 180
+
+      alphas += 1
+      MyWorkers::WorkerAlpha.perform_async(alphas)
     end
 
     puts "client done!"
